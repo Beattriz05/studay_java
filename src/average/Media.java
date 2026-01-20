bb2aff42fabc21b03373d55f39e7217ca393a00b
Beatriiz05 <anabeatrizdasilva263@gmail.com>
2026-01-20 13:38:53 -0300

    🪻average

--- src/average/Media.java
+++ src/average/Media.java
@@ -1,38 +1,29 @@
-void main() {
-    double nota1 = 8.9;
-    double nota2 = 7.5;
-    double media1 = (nota1 + nota2) / 2;
+package average;
 
-    // %.2f limita a 2 casas decimais. %n pula a linha.
-    System.out.printf("Média 1: %.2f%n", media1);
-
-    double media2 = calcularSegundaMedia(); // usado para puxar os métodos e guarda eles
-    double media3 = calcularTerceiraMedia();
-
-    double mediaGlobal = (media1 + media2 + media3) / 3; // usado para somar as três médias
-
-    IO.println("--- Resultado Final ---");
-    System.out.printf("Média Global: %.2f%n", mediaGlobal);
-}
-
-public static double calcularSegundaMedia() { // para calcular as médias juntas troquei o void por double
-    double nota1 = -5.8;
-    double nota2 = 8.0;
-
-    double media2 = (nota1 + nota2) / 2;
-
-    System.out.printf("Média 2: %.2f%n", media2);
-
-    return media2; // Envia o valor de volta para o main
-}
-
-public static double calcularTerceiraMedia() {
-    double nota1 = 5.0;
-    double nota2 = 9.0;
-
-    double media3 = (nota1 + nota2) / 2;
-
-    System.out.printf("average.Media 3: %.2f%n", media3);
-
-    return media3;
+public class Media {
+    public static void main (String[] args){
+        double nota1 = 8.9;
+        double nota2 = 7.5;
+        double media1 = (nota1+nota2) /2;
+        System.out.printf("Média 1: %.2f%n", media1);
+        double media2 = calcularSegundaMedia();
+        double media3 = calcularTerceiraMedia();
+        double mediaGlobal = (media1 + media2 + media3) / 3;
+        System.out.println("---Resultado Final---");
+        System.out.printf("Média Global: %.2f%n", mediaGlobal);
+    }
+    public static double calcularSegundaMedia() {
+        double nota1 = -5.8;
+        double nota2 = 8.0;
+        double media2 = (nota1 + nota2) / 2;
+        System.out.printf("Média 2: %.2f%n", media2);
+        return media2;
+    }
+    public static double calcularTerceiraMedia() {
+        double nota1 = 5.0;
+        double nota2 = 9.0;
+        double media3 = (nota1+nota2) / 2;
+        System.out.printf("Média 3: %.2f%n", media3);
+        return media3;
+    }
 }
